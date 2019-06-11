4ccc62b1fba91a65a78f02bb0a1760fc0d45ae0b
Swarnalata Keshavan <swarna_latha@hotmail.com>
2019-06-10 19:21:39 -0700

    Completed setting up webpack-dev-server and babel

--- src/index.js
+++ src/index.js
@@ -1,4 +1,36 @@
-var $ = require("jquery")
+import atd from './add_to_dom';
 
-var root = $('.root')
-root.html("<h1>Im cool</h1>")
+function delayed() {
+    console.log('1. Delayed function called');
+    const myPromise = new Promise(function(resolve, reject){
+        console.log('2. Promise  allback called');
+        setTimeout(function(){
+            console.log("3.SetTimeOut callback called");
+            resolve('This is the Info you requested');
+            //reject(new Error('Something went wrong'));
+        }, 3000);
+    } ); 
+    console.log("4.End of the delayed()");
+ return myPromise;     
+}
+
+async function handlePromiseNew() 
+{
+    const resp = await delayed();
+    console.log("Delyed reponse:", resp);
+}
+
+function handlePromiseOld()
+{
+const delayedString = delayed().then(function(resp){
+    console.log("Then callback called ");
+    console.log("Delayed response",resp);
+}).catch((err)=> {
+    console.log("Catch called",err.message);
+});
+console.log("Delayed String",delayedString);
+}
+
+//handlePromiseOld();
+
+handlePromiseNew();
